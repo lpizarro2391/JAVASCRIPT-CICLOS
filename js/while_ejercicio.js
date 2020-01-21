517688240effc25fd139fa8c20d7bf251c7967b9
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-01-21 10:34:09 -0600

    while se evalua la condicion antes de hacer la condicion, i con valor 11, no hay iteracion.

---
+++ js/while_ejercicio.js
@@ -0,0 +1,12 @@
+var lista_nombres = ["Camilo","Eduardo","Natalia","Juan","Alberto","Milton","Guillermo","Mateo","Maria","Pedro"];
+
+var i = 5;
+
+var resultado = "";
+
+while (i<10) {
+  resultado = resultado + lista_nombres[i] + "\n";
+  i = i + 1;
+};
+
+alert(resultado);
