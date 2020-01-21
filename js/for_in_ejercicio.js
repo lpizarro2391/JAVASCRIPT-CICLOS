fb8e8b8ae242043a902ec0e51bb2b032387c2cc5
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-01-21 11:00:00 -0600

    for_in solo debemos asignarle el objeto sobre el  que queremos iterar, en este caso la lista de nombres, se asume que queremos recojerlos uno a uno, no es necesario especifiar las demas sentencias.

---
+++ js/for_in_ejercicio.js
@@ -0,0 +1,9 @@
+var lista_nombres = ["Camilo","Eduardo","Natalia","Juan","Alberto","Milton","Guillermo","Mateo","Maria","Pedro"];
+
+var resultado = "";
+
+for (nombre_temp in lista_nombres) {
+resultado = resultado + lista_nombres[nombre_temp]+ "\n";
+};
+
+alert (resultado);
