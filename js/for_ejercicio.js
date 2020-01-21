8d50e3aa5efe3dba228dd97fffdaf0ae4c4b34ee
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-01-21 10:39:46 -0600

    var for, definimos 3 sentneicas, 1. valor inicial indice, 2. condicion ( i no puede tener valor mayor a 10)3. aumentar el valor de indice en una unidad.

---
+++ js/for_ejercicio.js
@@ -0,0 +1,9 @@
+var lista_nombres = ["Camilo","Eduardo","Natalia","Juan","Alberto","Milton","Guillermo","Mateo","Maria","Pedro"];
+
+var resultado = "";
+
+for(var i =5; i<10; i ++ ){
+  resultado = resultado + lista_nombres[i] + "\n"
+}
+
+alert (resultado);
